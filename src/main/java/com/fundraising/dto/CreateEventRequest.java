04493c9b82c68909e4e63524ace3d9888bfaacea
Oskar Karimov <oskarkarimovoskar@gmail.com>
2025-06-16 13:54:01 +0200

    Add DTO classes for requests and responses

--- src/main/java/com/fundraising/dto/CreateEventRequest.java
+++ src/main/java/com/fundraising/dto/CreateEventRequest.java
@@ -1,4 +1,26 @@
 package com.fundraising.dto;
 
+import com.fundraising.enums.Currency;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class CreateEventRequest {
+    @NotBlank(message = "Event name is required")
+    private String name;
+
+    @NotNull(message = "Currency is required")
+    private Currency currency;
+
+    public CreateEventRequest() {}
+
+    public CreateEventRequest(String name, Currency currency) {
+        this.name = name;
+        this.currency = currency;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Currency getCurrency() { return currency; }
+    public void setCurrency(Currency currency) { this.currency = currency; }
 }
--- src/main/java/com/fundraising/dto/FundraisingEventDTO.java
+++ src/main/java/com/fundraising/dto/FundraisingEventDTO.java
@@ -1,4 +1,32 @@
 package com.fundraising.dto;
 
+import com.fundraising.enums.Currency;
+import java.math.BigDecimal;
+
 public class FundraisingEventDTO {
+    private Long id;
+    private String name;
+    private BigDecimal balance;
+    private Currency currency;
+
+    public FundraisingEventDTO() {}
+
+    public FundraisingEventDTO(Long id, String name, BigDecimal balance, Currency currency) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.currency = currency;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public BigDecimal getBalance() { return balance; }
+    public void setBalance(BigDecimal balance) { this.balance = balance; }
+
+    public Currency getCurrency() { return currency; }
+    public void setCurrency(Currency currency) { this.currency = currency; }
 }
