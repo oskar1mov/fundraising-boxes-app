7b1009277c973f2e416d2e4a83320ea550c2aa91
Oskar Karimov <oskarkarimovoskar@gmail.com>
2025-06-16 14:04:09 +0200

    Add service classes for business logic

--- src/main/java/com/fundraising/service/FundraisingEventService.java
+++ src/main/java/com/fundraising/service/FundraisingEventService.java
@@ -1,4 +1,43 @@
 package com.fundraising.service;
 
+import com.fundraising.dto.CreateEventRequest;
+import com.fundraising.dto.FundraisingEventDTO;
+import com.fundraising.entity.FundraisingEvent;
+import com.fundraising.exception.DuplicateEventNameException;
+import com.fundraising.mapper.FundraisingEventMapper;
+import com.fundraising.repository.FundraisingEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FundraisingEventService {
+
+    @Autowired
+    private FundraisingEventRepository fundraisingEventRepository;
+
+    @Autowired
+    private FundraisingEventMapper mapper;
+
+    public FundraisingEventDTO createEvent(CreateEventRequest request) {
+        if (fundraisingEventRepository.existsByNameIgnoreCase(request.getName())) {
+            throw new DuplicateEventNameException(
+                    "An event with the name '" + request.getName() + "' already exists");
+        }
+
+        FundraisingEvent entity = mapper.toEntity(request);
+        FundraisingEvent savedEntity = fundraisingEventRepository.save(entity);
+        return mapper.toDto(savedEntity);
+    }
+
+    public List<FundraisingEventDTO> getAllEvents() {
+        List<FundraisingEvent> entities = fundraisingEventRepository.findAll();
+        return mapper.toDtoList(entities);
+    }
+
+    public Optional<FundraisingEventDTO> getEventById(Long id) {
+        return fundraisingEventRepository.findById(id)
+                .map(mapper::toDto);
+    }
 }
